6c7c78abf65a757fab36fa3f81b9d2c143e03bfc
Rampagerushtc <rampagerushtc@gmail.com>
2019-08-15 11:38:00 +0500

    The project structure has refactored

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,9 +3,9 @@ import './app-header.css';
 
 const AppHeader = () => {
     return (
-    <div className="app-header">
+    <div className="app-header d-flex">
         <h1>My Todo List</h1>
-        <p>3 more to do, 0 done</p>
+        <h2>3 more to do, 0 done</h2>
     </div>
     );
 }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TodoListItem from './todo-list-item';
-import './todo-list.css';
+import TodoListItem from '../todo-list-item';
+import '../todo-list/todo-list.css';
 
 const TodoList = ({ todos }) => {
     
