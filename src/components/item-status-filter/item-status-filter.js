e13ccedf7489cb76b740fab39154841ad2183b5a
rampagerushtc <rampagerushtc@gmail.com>
2019-08-15 22:00:17 +0500

    some refactor

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
+export default class ItemStatusFilter extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button className="btn btn-primary">All</button>
+                <button className="btn btn-outline-secondary">Active</button>
+                <button className="btn btn-outline-secondary">Done</button>
+            </div>
 
-    return (
-        <div className="btn-group">
-            <button className="btn btn-primary">All</button>
-            <button className="btn btn-outline-secondary">Active</button>
-            <button className="btn btn-outline-secondary">Done</button>
-        </div>
-
-    )
+        )
+    }
 }
-
-export default ItemStatusFilter;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css'
 
-const TodoListItem = ( { label, important = false } ) => {
-    
-    const style = {
-        color: important ? 'rgb(32, 95, 190)' : 'black',
-        fontWeight: important ? '1000' : '500'
+export default class TodoListItem extends Component {
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
     }
 
-    return (
-    <div>
-        <span   className="todo-list-item"
-            style = { style }> 
-            { label }
-        </span>
-        <button className="btn btn-trash btn-outline-danger">
-            <i className="fa fa-trash-o "></i>
-        </button>
-        <button className="btn btn-exclamation btn-outline-success">
-            <i className="fa fa-exclamation "></i>
-        </button>
-    </div>
 
-    );
-    
+    render() {
+
+        const { label, important = false } = this.props;
+        const style = {
+            color: important ? 'rgb(32, 95, 190)' : 'black',
+            fontWeight: important ? '1000' : '500'
+        }
+
+        return (
+            <span className="todo-list-item">
+                <span className="todo-list-item-label"
+                    style={style}
+                    onClick={ this.onLabelClick }>
+                       
+                    {label}
+                </span>
+                <button className="btn btn-trash btn-outline-danger">
+                    <i className="fa fa-trash-o "></i>
+                </button>
+                <button className="btn btn-exclamation btn-outline-success">
+                    <i className="fa fa-exclamation "></i>
+                </button>
+            </span>
+
+        );
+
+    }
 }
 
-export default TodoListItem;
+
