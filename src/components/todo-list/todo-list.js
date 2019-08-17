1dbff6718c9e1dfd58c936a607d8a7b06c923ce7
Rampagerushtc <rampagerushtc@gmail.com>
2019-08-17 14:10:51 +0500

    added onDeleted func

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,22 +2,25 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import '../todo-list/todo-list.css';
 
-const TodoList = ({ todos }) => {
-    
+const TodoList = ({ todos, onDeleted }) => {
+
     const elements = todos.map((item) => {
 
-        const {id, ...itemProps} = item;
+        const { id, ...itemProps } = item;
 
         return (
-        <li key={id} className="list-group-item">
-            <TodoListItem {...itemProps} />
-        </li>
+            <li key={id} className="list-group-item">
+                <TodoListItem
+                    {...itemProps}
+                    onDeleted={() => onDeleted(id)}
+                />
+            </li>
         )
     });
     return (
-    <ul className="list-group todo-list">    
-        { elements }
-    </ul>
+        <ul className="list-group todo-list">
+            {elements}
+        </ul>
     );
 }
 
