f272c83eb0ac13aeaf0f3fd5f686a10794f6302e
Rampagerushtc <rampagerushtc@gmail.com>
2019-08-17 14:05:08 +0500

    refactor app as class; added state and delete func

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
+export default class App extends Component {
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: true, id: 2 },
+            { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    }
 
-const App = () => {
-    
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 },
-    ];
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+            return {
+                todoData: newArray
+            }
+        })
+    }
 
-    return (
-    <div>
-        <AppHeader />
-        <SearchPanel />
-        <ItemStatusFilter />
-        <TodoList todos={todoData}/>
-    </div>
-    );
+    render() {
+        return (
+            <div>
+                <AppHeader />
+                <SearchPanel />
+                <ItemStatusFilter />
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}
+                />
+            </div >
+        );
+    }
 }
-
-
-
-export default App;
