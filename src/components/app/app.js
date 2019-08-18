c92b12b33ca8b923f9d52968b231021af69b52b0
rampagerushtc <rampagerushtc@gmail.com>
2019-08-18 15:34:18 +0500

    added addItem func, AddItemForm component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import AddItemForm from '../add-item-form';
 
 export default class App extends Component {
     state = {
@@ -23,6 +24,26 @@ export default class App extends Component {
         })
     }
 
+    addItem = () => {
+        this.setState( ({todoData}) => {
+            if (todoData.length === 0) {
+                return {
+                    todoData: [{ label: 'Make Awesome App', important: true, id: 1}]
+                }
+            } else {    
+                function makeNewArray(todoData) {
+                    const arrayOfId = todoData.map((el)=> el.id).sort();
+                    const newArray = [...todoData, { label: 'Make Awesome App', important: true, id: arrayOfId[arrayOfId.length-1]+1}];
+                    return newArray;
+                }
+                const newArray = makeNewArray(todoData);
+                return {
+                    todoData: newArray
+                }
+            }
+        })
+    }
+
     render() {
         return (
             <div>
@@ -33,6 +54,8 @@ export default class App extends Component {
                     todos={this.state.todoData}
                     onDeleted={this.deleteItem}
                 />
+                <AddItemForm 
+                    addItem={this.addItem}/>
             </div >
         );
     }
